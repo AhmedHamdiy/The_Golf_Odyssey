aefefc574e79a39d290a996e556e57bbd4af9d6d
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2025-04-30 22:20:38 +0300

    Physics: Add initial files

---
+++ source/common/systems/physics.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "../ecs/world.hpp"
+#include "../components/physics.hpp"
+
+#include <glm/glm.hpp>
+
+namespace our
+{
+    class PhysicsSystem {
+        public:
+    };
+}
