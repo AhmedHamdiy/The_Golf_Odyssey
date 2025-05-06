09deaec13a46eeab47224d70c634cdf6eede0e32
somiaelshemy <somiaelshemy22@gmail.com>
2025-05-07 00:40:51 +0300

    fix: all merge conflicts

--- source/states/play-state.hpp
+++ source/states/play-state.hpp
@@ -31,7 +31,7 @@ const int FELL_THRESHOLD = -15;
 const glm::vec3 CAMERA_OFFSET(0.0f, 5.0f, 10.0f);
 
 // This state shows how to use the ECS framework and deserialization.
-class Playstate : public our::State {
+class Playstate: public our::State {
 
     our::World world;
     our::ForwardRenderer renderer;
@@ -49,12 +49,9 @@ class Playstate : public our::State {
     void getNecessaryComponents(our::CameraComponent *&camera, our::Entity *&golfBall,
                                 our::Entity *&arrow) {
         for (auto entity : world.getEntities()) {
-            if (entity->name == "ball")
-                golfBall = entity;
-            else if (entity->name == "arrow")
-                arrow = entity;
-            else if (!camera)
-                camera = entity->getComponent<our::CameraComponent>();
+            if (entity->name == "ball") golfBall = entity;
+            else if (entity->name == "arrow") arrow = entity;
+            else if(!camera) camera = entity->getComponent<our::CameraComponent>();  
         }
     }
 
@@ -156,22 +153,21 @@ class Playstate : public our::State {
     void autoRelease(){
         glm::vec2 dragEnd = getApp()->getMouse().getMousePosition();
         glm::vec2 dragVec = dragEnd - dragStart;
+        
+        if(glm::length(dragVec) == 0.0f) return;
+        if(glm::length(dragVec) >= MAX_VELOCITY) onMouseButtonEvent(GLFW_MOUSE_BUTTON_LEFT, GLFW_RELEASE, 0);
 
-        if (glm::length(dragVec) == 0.0f)
-            return;
-        if (glm::length(dragVec) >= MAX_VELOCITY)
-            onMouseButtonEvent(GLFW_MOUSE_BUTTON_LEFT, GLFW_RELEASE, 0);
     }
 
     void onInitialize() override {
         // First of all, we get the scene configuration from the app config
-        auto &config = getApp()->getConfig()["scene"];
+        auto& config = getApp()->getConfig()["scene"];
         // If we have assets in the scene config, we deserialize them
-        if (config.contains("assets")) {
+        if(config.contains("assets")){
             our::deserializeAllAssets(config["assets"]);
         }
         // If we have a world in the scene config, we use it to populate our world
-        if (config.contains("world")) {
+        if(config.contains("world")){
             world.deserialize(config["world"]);
         }
         // we add the physics system to the world
@@ -202,22 +198,20 @@ class Playstate : public our::State {
         if(elapsed >= MAX_TIME || (strokesNum > MAX_STROKES && !won) || won || fell) return;
 
         movementSystem.update(&world, (float)deltaTime, physicsSystem.getRigidBodies());
-        if (!ballDragging)
-            cameraController.update(&world, (float)deltaTime);
+        if(!ballDragging) cameraController.update(&world, (float)deltaTime);
         // updateCameraPosition();
-        if (ballDragging)
-            updateArrow();
+        if(ballDragging) updateArrow();
         autoRelease();
         // updateBallVelocity(deltaTime);
         physicsSystem.update(&world, (float)deltaTime);
-
+        
         // And finally we use the renderer system to draw the scene
         renderer.render(&world);
 
         // Get a reference to the keyboard object
-        auto &keyboard = getApp()->getKeyboard();
+        auto& keyboard = getApp()->getKeyboard();
 
-        if (keyboard.justPressed(GLFW_KEY_ESCAPE)) {
+        if(keyboard.justPressed(GLFW_KEY_ESCAPE)){
             // If the escape  key is pressed in this frame, go to the play state
             getApp()->changeState("menu");
         }
@@ -226,8 +220,7 @@ class Playstate : public our::State {
     void onDestroy() override {
         // Don't forget to destroy the renderer
         renderer.destroy();
-        // On exit, we call exit for the camera controller system to make sure that the mouse is
-        // unlocked
+        // On exit, we call exit for the camera controller system to make sure that the mouse is unlocked
         cameraController.exit();
         // Clear the world
         world.clear();
@@ -235,28 +228,23 @@ class Playstate : public our::State {
         our::clearAllAssets();
     }
 
-    virtual void onMouseButtonEvent(int button, int action, int mods) override {
-        if (button == GLFW_MOUSE_BUTTON_LEFT) {
-            our::Entity *golfBall = nullptr;
-            our::CameraComponent *camera = nullptr;
+    virtual void onMouseButtonEvent(int button, int action, int mods)override{
+        if(button == GLFW_MOUSE_BUTTON_LEFT){
+            our::Entity* golfBall = nullptr;
+            our::CameraComponent* camera = nullptr;
             our::MovementComponent *golfMovementComponent = nullptr;
             our::Entity *arrow = nullptr;
-            getNecessaryComponents(camera, golfBall, arrow);
-            btRigidBody *body = physicsSystem.getRigidBodies()[golfBall];
-
-            if (golfBall) {
+            getNecessaryComponents(camera,golfBall,arrow);
+            btRigidBody* body = physicsSystem.getRigidBodies()[golfBall];
+            
+            if(golfBall)
                 golfMovementComponent = golfBall->getComponent<our::MovementComponent>();
-                glm::vec3 ballPos = golfBall->localTransform.position;
-                std::cout << "Ball Position: (" << ballPos.x << ", " << ballPos.y << ", "
-                          << ballPos.z << ")\n";
-            }
-            if (glm::length(body->getLinearVelocity().length()) > MIN_VELOCITY)
+            if(glm::length(body->getLinearVelocity().length()) > MIN_VELOCITY)
                 return;
-            if (action == GLFW_PRESS) {
+            if(action == GLFW_PRESS){
                 glm::vec2 windowSize = renderer.windowSize;
                 glm::vec3 ballWorldPos = golfBall->localTransform.position;
-                glm::vec4 clipSpace = (camera->getProjectionMatrix(windowSize)) *
-                                      (camera->getViewMatrix()) * glm::vec4(ballWorldPos, 1.0f);
+                glm::vec4 clipSpace = (camera->getProjectionMatrix(windowSize)) * (camera->getViewMatrix()) * glm::vec4(ballWorldPos, 1.0f);
                 clipSpace /= clipSpace.w;
                 float x = (clipSpace.x * 0.5f + 0.5f) * windowSize.x;
                 float y = (1.0f - (clipSpace.y * 0.5f + 0.5f)) * windowSize.y;
@@ -264,46 +252,41 @@ class Playstate : public our::State {
                 glm::vec2 ballPos = glm::vec2(x, y);
                 glm::vec2 mousePos = getApp()->getMouse().getMousePosition();
 
-                float distance = glm::distance(ballPos, mousePos);
-                if (distance < MOUSE_TO_BALL_THRESHOLD) {
+                float distance = glm::distance(ballPos,mousePos);
+                if(distance < MOUSE_TO_BALL_THRESHOLD){
                     dragStart = ballPos;
                     ballDragging = true;
                     strokesNum++;
                 }
-            } else if (action == GLFW_RELEASE && ballDragging) {
-                arrow->localTransform.scale = glm::vec3(0, 0, 0);
+            }else if(action == GLFW_RELEASE && ballDragging){
+                arrow->localTransform.scale = glm::vec3(0,0,0);
 
                 ballDragging = false;
                 glm::vec2 dragEnd = getApp()->getMouse().getMousePosition();
                 glm::vec2 dragVec = dragEnd - dragStart;
-
-                if (glm::length(dragVec) == 0.0f)
-                    return;
-                if (glm::length(dragVec) >= MAX_VELOCITY)
-                    dragVec = MAX_VELOCITY * glm::normalize(dragVec);
+                
+                if(glm::length(dragVec) == 0.0f) return;
+                if(glm::length(dragVec) >= MAX_VELOCITY) dragVec = MAX_VELOCITY * glm::normalize(dragVec);
 
                 glm::mat4 viewMatrix = camera->getViewMatrix();
-                glm::vec3 cameraRight =
-                    glm::vec3(viewMatrix[0][0], viewMatrix[1][0], viewMatrix[2][0]);
-                glm::vec3 cameraUp =
-                    glm::vec3(viewMatrix[0][1], viewMatrix[1][1], viewMatrix[2][1]);
-                glm::vec3 cameraForward =
-                    glm::vec3(viewMatrix[0][2], viewMatrix[1][2], viewMatrix[2][2]);
+                glm::vec3 cameraRight = glm::vec3(viewMatrix[0][0], viewMatrix[1][0], viewMatrix[2][0]);
+                glm::vec3 cameraUp = glm::vec3(viewMatrix[0][1], viewMatrix[1][1], viewMatrix[2][1]);
+                glm::vec3 cameraForward = glm::vec3(viewMatrix[0][2], viewMatrix[1][2], viewMatrix[2][2]);
 
                 cameraForward.y = 0;
                 cameraForward = glm::normalize(cameraForward);
                 glm::vec3 velocity = (-dragVec.x * cameraRight) + (-dragVec.y * cameraForward);
                 velocity *= VELOCITY_FACTOR;
-                std::cout << "velocity: " << glm::length(velocity) << "\n";
+                std::cout<<"velocity: "<<glm::length(velocity)<<"\n";
 
-                if (golfMovementComponent) {
+                if(golfMovementComponent){
                     golfMovementComponent->linearVelocity = velocity;
-                    glm::vec3 rotationAxis =
-                        glm::normalize(glm::cross(velocity, glm::vec3(0, 1, 0)));
+                    glm::vec3 rotationAxis = glm::normalize(glm::cross(velocity, glm::vec3(0, 1, 0)));
                     float angularSpeed = glm::length(velocity) / BALL_RADIUS;
                     golfMovementComponent->angularVelocity = angularSpeed * rotationAxis;
                 }
             }
         }
     }
+
 };
